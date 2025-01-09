7c93b32c45791a203082443f5fa23f1060d19550
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-01-09 15:02:57 +0100

    ADD: inzializzazione del progetto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
