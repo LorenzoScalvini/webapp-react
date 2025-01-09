23fdb3687f684e0b271b12664547a5a6bb0897fd
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-01-09 18:04:33 +0100

    ADD:  updated footer to a newer one and more complex

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,63 @@
+import React from "react";
 import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p>&copy; {new Date().getFullYear()} MovieApp. All rights reserved.</p>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <a href="/terms" className={styles.navLink}>
-              Terms
-            </a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="/privacy" className={styles.navLink}>
-              Privacy
-            </a>
-          </li>
-          <li className={styles.navItem}>
-            <a href="/contact" className={styles.navLink}>
-              Contact
-            </a>
-          </li>
-        </ul>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <h4 className={styles.title}>MovieApp</h4>
+            <p className={styles.description}>
+              Your go-to app for movie reviews and ratings.
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h4 className={styles.title}>Links</h4>
+            <ul className={styles.navList}>
+              <li className={styles.navItem}>
+                <a href="/terms" className={styles.navLink}>
+                  Terms of Use
+                </a>
+              </li>
+              <li className={styles.navItem}>
+                <a href="/privacy" className={styles.navLink}>
+                  Privacy Policy
+                </a>
+              </li>
+              <li className={styles.navItem}>
+                <a href="/contact" className={styles.navLink}>
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h4 className={styles.title}>Follow Us</h4>
+            <ul className={styles.socialList}>
+              <li className={styles.socialItem}>
+                <a href="https://facebook.com" className={styles.socialLink}>
+                  Facebook
+                </a>
+              </li>
+              <li className={styles.socialItem}>
+                <a href="https://twitter.com" className={styles.socialLink}>
+                  Twitter
+                </a>
+              </li>
+              <li className={styles.socialItem}>
+                <a href="https://instagram.com" className={styles.socialLink}>
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.copyright}>
+            &copy; {new Date().getFullYear()} MovieApp. All rights reserved.
+          </p>
+        </div>
       </div>
     </footer>
   );
