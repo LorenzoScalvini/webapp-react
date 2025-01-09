828eb638fd71aaf7ed16a7b7e653a8ffab54e131
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-01-09 18:11:20 +0100

    ADD: responsive navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,37 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <Link className={styles.brand} to="/">
           MovieApp
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
+        <button className={styles.toggler} onClick={toggleMenu}>
+          ☰
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
+        <div className={`${styles.navMenu} ${isMenuOpen ? styles.active : ""}`}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <Link className={styles.navLink} to="/">
                 Home
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
+            <li className={styles.navItem}>
+              <Link className={styles.navLink} to="/about">
                 About
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+            <li className={styles.navItem}>
+              <Link className={styles.navLink} to="/contact">
                 Contact
               </Link>
             </li>
